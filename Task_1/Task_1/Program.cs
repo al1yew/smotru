33faead573d37641691ed2078bfd7133ad0592ef
al1yew <98798528+al1yew@users.noreply.github.com>
2022-02-20 19:06:34 +0400

    axssd
    asd

---
+++ Task_1/Task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salam, ashagidaki melumatlari daxil edin.");
+            Console.WriteLine("Username:");
+            string username = Console.ReadLine();
+            Console.WriteLine("Password:");
+            string password = Console.ReadLine();
+
+            User user = new User();  // teze user yaratdim, username ve password verdim 
+
+            user.Username = username;
+            user.Password = password;
+
+            Console.WriteLine(user.GetFullName());
+
+            //string word = "09 AZ az";   // spacein ascii si 32 di
+            //foreach (char item in word)
+            //{
+            //    Console.WriteLine((int)item);
+            //}
+
+        }
+    }
+}
---
+++ Task_1/Task_1/User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    class User
+    {
+        private string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                string error1 = "Daxil edilen username sehvdir, minimum 8 elementden ibaret olmalidir.";
+                if (value.Length >= 8)
+                {
+                    _username = value;
+                    return;
+                }
+                Console.WriteLine(error1);
+            }
+        }
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                string b = "Password minimum 8 elementden ibaret olmalidir";
+
+                if (value.Length >= 8)
+                {
+                    foreach (char item in value)
+                    {
+                        if (((int)item >= 48 && (int)item <= 57) && ((int)item >= 65 && (int)item <= 90) && ((int)item >= 97 && (int)item <= 122))
+                        {
+                            _password = value;
+                        }
+                    }
+                    return;
+                }
+                Console.WriteLine(b);
+            }
+        }
+        public string GetFullName()
+        {
+            return $"{Username},{Password} - sizin daxil etdiyiniz melumatdir";
+        }
+    }
+}
